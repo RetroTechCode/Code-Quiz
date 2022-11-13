337d59ffb89b50e8de885f25669333fbbd71fa73
Dan <danaaboston@gmail.com>
2022-11-12 21:55:43 -0500

    Add questions into an object within JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,58 @@
 var startButton = document.querySelector("#start");
 
-startButton.addEventListener("click", function() {
+var questions = {
+    questionOne: {
+        question: "Which tag would you use to link your Javasript file to your HTML?",
+        options: ["<link> in the head", "<link> in the body", "<script> in the head", "<script> in the body"],
+        answer: "<script> in the body",
+    },
+    questionTwo: {
+        question: "An array is stored within which of the following?",
+        options: ["[ ]", "( )", "< >", "{ }"],
+        answer: "[ ]",
+    },
+    questionThree: {
+        question: "An if statement is sometimes followed by a _____ statement.",
+        options: ["when", "then", "else", "why"],
+        answer: "else",
+    },
+    questionFour: {
+        question: "What does event.stopPropagation() do in Javascript?",
+        options: ["Prevents the default behaviors from occurring", "Prevents interacting through objects on the page", "Refreshes the page", "Nothing"],
+        answer: "Prevents interacting through objects on the page",
+    },
+    questionFive: {
+        question: "Which is the following is used when declaring an object?",
+        options: ["[ ]", "( )", "< >", "{ }"],
+        answer: "{ }",
+    },
+    questionSix: {
+        question: "_____ is used for strict equality.",
+        options: ["=", "==", "===", "!=="],
+        answer: "===",
+    },
+    questionSeven: {
+        question: "A boolean response can be _____ or _____.",
+        options: ["Yes, No", "0, 1", "True, False", "If, Then"],
+        answer: "True, False",
+    },
+    questionEight: {
+        question: "'Hello World!' is an example of a(n) _____.",
+        options: ["String", "Array", "Object", "Boolean"],
+        answer: "String",
+    },
+    questionNine: {
+        question: "What does event.preventDefault() do in Javascript?",
+        options: ["Prevents the default behaviors from occurring", "Prevents interacting through objects on the page", "Refreshes the page", "Nothing"],
+        answer: "Prevents the default behaviors from occurring",
+    },
+    questionTen: {
+        question: "Which of the following is used when declaring a function?",
+        options: ["[ ]", "( )", "< >", "{ }"],
+        answer: "( )",
+    }
+}
+
+startButton.addEventListener("click", function () {
     console.log("Begin!");
 });
