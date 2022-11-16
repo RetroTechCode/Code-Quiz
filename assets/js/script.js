3d163bf31018eff2dd8049d6625b8d98cde8d11a
Dan <danaaboston@gmail.com>
2022-11-16 16:18:56 -0500

    Fixed localStorage to store values properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -130,103 +130,106 @@ function scoreScreen() {
     scoreEl.textContent = userScore;
 
     var saveScoreBtn = document.getElementById("saveScore");
-    saveScoreBtn.addEventListener("click", saveScore, highScoreScreen);
+    saveScoreBtn.addEventListener("click", saveScore);
 };
 
 function saveScore() {
     var userNameEl = document.getElementById("userName");
     var userName = userNameEl.value;
 
-    if (userName !== '') {
-        var scores = JSON.parse(window.localStorage.getItem("scores")) || [];
+    var scores = JSON.parse(window.localStorage.getItem("scores")) || [ ];
 
-        var userInfo = {
-            userName: userName,
-            userScore: userScore,
-        };
-        
-        scores.push(userInfo);
-        window.localStorage.setItem("scores", JSON.stringify(scores));
-    }
+    console.log(scores);
+
+    var userInfo = {
+        userName: userName,
+        userScore: userScore,
+    };
+
+    scores.push(userInfo);
+    window.localStorage.setItem("scores", JSON.stringify(scores));
+
+    highScoreScreen();
 }
 
-    function highScoreScreen() {
-        console.log("highScoreScreen ran");
+function highScoreScreen() {
+    console.log("highScoreScreen ran");
 
-        // Make sure that any page the user could be on is cleared
-        welcomeScreenEl.setAttribute("class", "hidden");
-        questionScreenEl.setAttribute("class", "hidden");
-        scoreScreenEl.setAttribute("class", "hidden");
+    // Make sure that any page the user could be on is cleared
+    welcomeScreenEl.setAttribute("class", "hidden");
+    questionScreenEl.setAttribute("class", "hidden");
+    scoreScreenEl.setAttribute("class", "hidden");
 
-        // Unhide highscore screen
-        highScoreScreenEl.removeAttribute("class");
+    // Unhide highscore screen
+    highScoreScreenEl.removeAttribute("class");
 
-        // Hide the view high scores button
-        viewHighBtn.setAttribute("class", "hidden");
+    // Hide the view high scores button
+    viewHighBtn.setAttribute("class", "hidden");
 
-        // Verify the start over button is shown
-        returnHomeEl.removeAttribute("class")
+    // Verify the start over button is shown
+    returnHomeEl.removeAttribute("class")
 
+
+}
+
+function returnHome() {
+    window.location.reload();
+}
+
+startButton.addEventListener("click", startQuiz);
+returnHomeEl.addEventListener("click", returnHome);
+viewHighBtn.addEventListener("click", highScoreScreen);
+optionsEl.addEventListener("click", chooseOption);
+
+var questions = [
+    {
+        question: "Which tag would you use to link your Javasript file to your HTML?",
+        options: ["<link> in the head", "<link> in the body", "<script> in the head", "<script> in the body"],
+        answer: "<script> in the body",
+    },
+    {
+        question: "An array is stored within which of the following?",
+        options: ["[ ]", "( )", "< >", "{ }"],
+        answer: "[ ]",
+    },
+    {
+        question: "An if statement is sometimes followed by a _____ statement.",
+        options: ["when", "then", "else", "why"],
+        answer: "else",
+    },
+    {
+        question: "What does event.stopPropagation() do in Javascript?",
+        options: ["Prevents the default behaviors from occurring", "Prevents interacting through objects on the page", "Refreshes the page", "Nothing"],
+        answer: "Prevents interacting through objects on the page",
+    },
+    {
+        question: "Which is the following is used when declaring an object?",
+        options: ["[ ]", "( )", "< >", "{ }"],
+        answer: "{ }",
+    },
+    {
+        question: "_____ is used for strict equality.",
+        options: ["=", "==", "===", "!=="],
+        answer: "===",
+    },
+    {
+        question: "A boolean response can be _____ or _____.",
+        options: ["Yes, No", "0, 1", "True, False", "If, Then"],
+        answer: "True, False",
+    },
+    {
+        question: "'Hello World!' is an example of a(n) _____.",
+        options: ["String", "Array", "Object", "Boolean"],
+        answer: "String",
+    },
+    {
+        question: "What does event.preventDefault() do in Javascript?",
+        options: ["Prevents the default behaviors from occurring", "Prevents interacting through objects on the page", "Refreshes the page", "Nothing"],
+        answer: "Prevents the default behaviors from occurring",
+    },
+    {
+        question: "Which of the following is used when declaring a function?",
+        options: ["[ ]", "( )", "< >", "{ }"],
+        answer: "( )",
     }
-
-    function returnHome() {
-        window.location.reload();
-    }
-
-    startButton.addEventListener("click", startQuiz);
-    returnHomeEl.addEventListener("click", returnHome);
-    viewHighBtn.addEventListener("click", highScoreScreen);
-    optionsEl.addEventListener("click", chooseOption);
-
-    var questions = [
-        {
-            question: "Which tag would you use to link your Javasript file to your HTML?",
-            options: ["<link> in the head", "<link> in the body", "<script> in the head", "<script> in the body"],
-            answer: "<script> in the body",
-        },
-        {
-            question: "An array is stored within which of the following?",
-            options: ["[ ]", "( )", "< >", "{ }"],
-            answer: "[ ]",
-        },
-        {
-            question: "An if statement is sometimes followed by a _____ statement.",
-            options: ["when", "then", "else", "why"],
-            answer: "else",
-        },
-        {
-            question: "What does event.stopPropagation() do in Javascript?",
-            options: ["Prevents the default behaviors from occurring", "Prevents interacting through objects on the page", "Refreshes the page", "Nothing"],
-            answer: "Prevents interacting through objects on the page",
-        },
-        {
-            question: "Which is the following is used when declaring an object?",
-            options: ["[ ]", "( )", "< >", "{ }"],
-            answer: "{ }",
-        },
-        {
-            question: "_____ is used for strict equality.",
-            options: ["=", "==", "===", "!=="],
-            answer: "===",
-        },
-        {
-            question: "A boolean response can be _____ or _____.",
-            options: ["Yes, No", "0, 1", "True, False", "If, Then"],
-            answer: "True, False",
-        },
-        {
-            question: "'Hello World!' is an example of a(n) _____.",
-            options: ["String", "Array", "Object", "Boolean"],
-            answer: "String",
-        },
-        {
-            question: "What does event.preventDefault() do in Javascript?",
-            options: ["Prevents the default behaviors from occurring", "Prevents interacting through objects on the page", "Refreshes the page", "Nothing"],
-            answer: "Prevents the default behaviors from occurring",
-        },
-        {
-            question: "Which of the following is used when declaring a function?",
-            options: ["[ ]", "( )", "< >", "{ }"],
-            answer: "( )",
-        }
-    ]
+]
